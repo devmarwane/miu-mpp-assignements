a82199138f856dac6588b4291bab684ba004b9b7
Temuulen <temuulenkhuchit01@gmail.com>
2024-09-06 17:22:55 -0500

    Updated

--- assignement-4/Lab4/src/Main.java
+++ assignement-4/Lab4/src/Main.java
@@ -7,13 +7,12 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Order> list = new ArrayList();
-        list.add(new Order("100", LocalDate.of(2023, 2, 1),200));
-        list.add(new Order("100",LocalDate.of(2023, 2, 10),100));
-        Commissioned cm = new Commissioned("123",0.8,500,list);
-        Employee[] emp = { new Salaried("121",4000), new Hourly("122",15.67,20),cm};
-        System.out.println("Hello");
-        for(Employee e :emp){
-            e.print(3,2023);
+        list.add(new Order("100", LocalDate.of(2023, 2, 1), 200));
+        list.add(new Order("100", LocalDate.of(2023, 2, 10), 100));
+        Commissioned cm = new Commissioned("123", 0.8, 500, list);
+        Employee[] emp = {new Salaried("121", 4000), new Hourly("122", 15.67, 20), cm};
+        for (Employee e : emp) {
+            e.print(3, 2023);
         }
     }
 }
