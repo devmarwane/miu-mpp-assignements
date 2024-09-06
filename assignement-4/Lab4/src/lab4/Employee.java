4829ae05a090f3bddbb56e347ee2ea04e30af460
Temuulen <temuulenkhuchit01@gmail.com>
2024-09-06 18:14:54 -0500

    Upgraded calcCompensation function

--- assignement-4/Lab4/src/lab4/Employee.java
+++ assignement-4/Lab4/src/lab4/Employee.java
@@ -16,7 +16,7 @@ public abstract class Employee {
     }
 
     public Paycheck calcCompensation(int month, int year) {
-        double grossPay = calcGrossPay();
+        double grossPay = calcGrossPay(month, year);
 
         double fica = 0.23 * grossPay;
         double state = 0.05 * grossPay;
@@ -27,5 +27,5 @@ public abstract class Employee {
         return new Paycheck(grossPay, fica, state, local, medicare, socialSecurity);
     }
 
-    public abstract double calcGrossPay();
+    public abstract double calcGrossPay(int month, int year);
 }
