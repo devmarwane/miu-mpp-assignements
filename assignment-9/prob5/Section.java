775abb359a7d870bb9a5b3dd72cf9f17a9e1f347
Temuulen <temuulenkhuchit01@gmail.com>
2024-09-20 17:19:14 -0500

    Prob 5

--- assignment-9/prob5/Section.java
+++ assignment-9/prob5/Section.java
@@ -6,15 +6,18 @@ import java.util.stream.Stream;
 
 public class Section {
 	public static Stream<String> streamSection(Stream<String> stream, int m, int n) {
-		return null; //implement 
+		return stream.skip(m)
+				.limit(n - m + 1);
 	}
-	
+
 	public static void main(String[] args) {
-		/* Make three calls for the streamSection() method with different inputs range for the m and n.              
+		/* Make three calls for the streamSection() method with different inputs range for the m and n.
 		   Use nextStream() method to supply the Stream input as a first argument in streamSection() method */
-	
+		System.out.println(streamSection(nextStream(), 0, 5).toList());
+		System.out.println(streamSection(nextStream(), 2, 3).toList());
+		System.out.println(streamSection(nextStream(), 3, 7).toList());
 	}
-	
+
 	//support method for the main method -- for testing
 	private static Stream<String> nextStream() {
 		return Arrays.asList("aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh", "iii").stream();
