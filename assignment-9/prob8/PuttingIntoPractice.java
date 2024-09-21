c6a1b709aa5f429379c366839fb4f1be93d23d7f
Temuulen <temuulenkhuchit01@gmail.com>
2024-09-20 21:36:53 -0500

    Prob 8 and 9

--- assignment-9/prob8/PuttingIntoPractice.java
+++ assignment-9/prob8/PuttingIntoPractice.java
@@ -1,46 +1,83 @@
+import javax.management.Query;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 
-public class PuttingIntoPractice{
-    public static void main(String ...args){    
+public class PuttingIntoPractice {
+    public static void main(String... args) {
         Trader raoul = new Trader("Raoul", "Cambridge");
-        Trader mario = new Trader("Mario","Milan");
-        Trader alan = new Trader("Alan","Cambridge");
-        Trader brian = new Trader("Brian","Cambridge");
-		
-		List<Transaction> transactions = Arrays.asList(
-            new Transaction(brian, 2011, 300), 
-            new Transaction(raoul, 2012, 1000),
-            new Transaction(raoul, 2011, 400),
-            new Transaction(mario, 2012, 710),	
-            new Transaction(mario, 2012, 700),
-            new Transaction(alan, 2012, 950)
-        );	
-        
-        
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
+        List<Trader> traders = Arrays.asList(raoul, mario, alan, brian);
+
+
         // Query 1: Find all transactions from year 2011 and sort them by value (small to high).
+        System.out.println("Query 1: Find all transactions from year 2011 and sort them by value (small to high).");
+        transactions.stream()
+                .filter(t -> t.getYear() >= 2011)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .forEach(System.out::println);
 
 
-        
         // Query 2: What are all the unique cities where the traders work?
-        
+        System.out.println("\nQuery 2: What are all the unique cities where the traders work?");
+        traders.stream()
+                .map(Trader::getCity)
+                .distinct()
+                .forEach(System.out::println);
 
         // Query 3: Find all traders from Cambridge and sort them by name.
-        
-   
-        
+        System.out.println("\nQuery 3: Find all traders from Cambridge and sort them by name.");
+        traders.stream()
+                .filter(t -> t.getCity().equals("Cambridge"))
+                .sorted(Comparator.comparing(Trader::getName))
+                .map(Trader::getName)
+                .forEach(System.out::println);
+
+
         // Query 4: Return a string of all traders names sorted alphabetically.
-        
+        System.out.println("\nQuery 4: Return a string of all traders names sorted alphabetically.");
+        System.out.println(traders.stream()
+                .map(Trader::getName)
+                .sorted()
+                .collect(Collectors.joining(", ")));
+
 
         // Query 5: Are there any trader based in Milan?
-        
-   
-     // Query 6: Update all transactions so that the traders from Milan are set to Cambridge.
-        
-        
+        System.out.println("\nQuery 5: Are there any trader based in Milan?");
+        traders.stream()
+                .filter(t -> t.getCity().equals("Milan"))
+                .map(Trader::getName)
+                .forEach(System.out::println);
+
+
+        // Query 6: Update all transactions so that the traders from Milan are set to Cambridge.
+        System.out.println("\nQuery 6: Update all transactions so that the traders from Milan are set to Cambridge.");
+        traders.stream()
+                .filter(t -> t.getCity().equals("Milan"))
+                .forEach(t -> t.setCity("Cambridge"));
+        transactions.forEach(System.out::println);
+
         // Query 7: What's the highest value in all the transactions?
+        System.out.println("\nQuery 7: What's the highest value in all the transactions?");
+        System.out.println(transactions.stream()
+                .map(Transaction::getValue)
+                .max(Integer::compareTo)
+                .get());
     }
 }
--- assignment-9/prob9/Dish.java
+++ assignment-9/prob9/Dish.java
@@ -1,4 +1,5 @@
 package lesson9.labsolns.prob10;
+
 import java.util.*;
 
 public class Dish {
@@ -31,7 +32,7 @@ public class Dish {
         return type;
     }
 
-    public enum Type { MEAT, FISH, OTHER }
+    public enum Type {MEAT, FISH, OTHER}
 
     @Override
     public String toString() {
@@ -39,13 +40,56 @@ public class Dish {
     }
 
     public static final List<Dish> menu =
-            Arrays.asList( new Dish("pork", false, 800, Dish.Type.MEAT),
-                           new Dish("beef", false, 700, Dish.Type.MEAT),
-                           new Dish("chicken", false, 400, Dish.Type.MEAT),
-                           new Dish("french fries", true, 530, Dish.Type.OTHER),
-                           new Dish("rice", true, 350, Dish.Type.OTHER),
-                           new Dish("season fruit", true, 120, Dish.Type.OTHER),
-                           new Dish("pizza", true, 550, Dish.Type.OTHER),
-                           new Dish("prawns", false, 400, Dish.Type.FISH),
-                           new Dish("salmon", false, 450, Dish.Type.FISH));
+            Arrays.asList(new Dish("pork", false, 800, Dish.Type.MEAT),
+                    new Dish("beef", false, 700, Dish.Type.MEAT),
+                    new Dish("chicken", false, 400, Dish.Type.MEAT),
+                    new Dish("french fries", true, 530, Dish.Type.OTHER),
+                    new Dish("rice", true, 350, Dish.Type.OTHER),
+                    new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                    new Dish("pizza", true, 550, Dish.Type.OTHER),
+                    new Dish("prawns", false, 400, Dish.Type.FISH),
+                    new Dish("salmon", false, 450, Dish.Type.FISH));
+
+    public static void main(String[] args) {
+        // Optional, anyMatch(), allMatch(), noneMatch(), findAny(), findFirst()
+
+        // Problem A. Is there any Vegetarian meal available (return type boolean)
+        System.out.println("Problem A. Is there any Vegetarian meal available (return type boolean)");
+        System.out.println(menu.stream()
+                .anyMatch(Dish::isVegetarian));
+
+
+        // Problem B. Is there any healthy menu have calories less than 1000 (return type boolean)
+        System.out.println("\nProblem B. Is there any healthy menu have calories less than 1000 (return type boolean)");
+        System.out.println(menu.stream()
+                .anyMatch(d -> d.getCalories() < 1000));
+
+
+        // Problem C. Is there any unhealthy menu have calories greater than 1000 (return type boolean)
+        System.out.println("\nProblem C. Is there any unhealthy menu have calories greater than 1000 (return type boolean)");
+        System.out.println(menu.stream()
+                .anyMatch(d -> d.getCalories() > 1000));
+
+
+        // Problem D. find and return the first item for the type of MEAT(return type Optional<Dish>)
+        System.out.println("\nProblem D. find and return the first item for the type of MEAT(return type Optional<Dish>)");
+        Optional<Dish> firstMeat = menu.stream()
+                .filter(d -> d.getType().equals(Type.MEAT))
+                .findFirst();
+        System.out.println(firstMeat);
+
+
+        // Problem E. calculateTotalCalories() in the menu using reduce. (return int)
+        System.out.println("\nProblem E. calculateTotalCalories() in the menu using reduce. (return int)");
+        System.out.println(menu.stream()
+                .reduce(0, (sum, dish) -> sum + dish.getCalories(), Integer::sum));
+
+
+        // Problem F. calculateTotalCaloriesMethodReference() in the menu using MethodReferences. (return int)
+        System.out.println("\nProblem F. calculateTotalCaloriesMethodReference() in the menu using MethodReferences. (return int)");
+        System.out.println(menu.stream()
+                .map(Dish::getCalories)
+                .reduce(0, Integer::sum));
+
+    }
 }
